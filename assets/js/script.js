ae533b38213499193d72af0878d00a74f3c38360
wujiaan2022 <emmawu2004@gmail.com>
2023-01-18 01:26:33 +0000

    fix some counting bugs, but still something cannot be fixed, like first click, score wnot show, and when click wood, score not show.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var yourScore = 0;
 var opponent;
 var opponentScore = 0;
 
-var choices = ["Metal", "Wood", "Water", "Fire", "Earth"];
+var choices = ["metal", "wood", "water", "fire", "earth"];
 
 window.onload = function () {
     for (let i = 0; i < 5; i++) {
@@ -18,90 +18,114 @@ window.onload = function () {
 function selectChoice () {
     you = this.id;
     document.getElementById("your-choice").src = you + ".png"
-}
 
- //random for oppponent
- opponent = choices[Math.floor(Math.random() * 5)]; 
- document.getElementById("opponent-choice").src = opponent + ".png";
+     //random for oppponent
+    opponent = choices[Math.floor(Math.random() * 5)]; 
+    document.getElementById("opponent-choice").src = opponent + ".png";
 
-     //check for winner
-     if (you == opponent) {
+    //check for winner
+    if (you == opponent) {
         yourScore += 1;
         opponentScore += 1;
-    }
-    else {
+     }
+     else {
         if (you == "metal") {
             if (opponent == "Wood") {
                 yourScore += 1;
             }
-            else if (opponent == "Water") {
+            else 
+            if (opponent == "Water") {
                 opponentScore += 1;
             }
-            else if (opponent == "fire") {
+            else 
+            if (opponent == "fire") {
                 opponentScore += 1;
             }
-            else if (opponent == "earth") {
+            else 
+            if (opponent == "earth") {
                 yourScore += 1;
-            } 
-        }
-        else if (you == "Water") {
-            if (opponent == "metal") {
-                yourScore += 1;
-            }
-            else if (opponent == "Wood") {
-                opponentScore += 1;
-            }
-            else if (opponent == "fire") {
-                yourScore += 1;
-            }
-            else if (opponent == "earth") {
-                opponentScore += 1;
-            } 
-        }
-        else if (you == "Wood") {
-            if (opponent == "metal") {
-                opponentScore += 1;
-            }
-            else if (opponent == "Water") {
-                yourScore += 1;
-            }
-            else if (opponent == "fire") {
-                opponentScore += 1;
-            }
-            else if (opponent == "earth") {
-                yourScore += 1;
-            } 
-        }
-        else if (you == "fire") {
-            if (opponent == "metal") {
-                yourScore += 1;
-            }
-            else if (opponent == "Water") {
-                opponentScore += 1;
-            }
-            else if (opponent == "wood") {
-                yourScore += 1;
-            }
-            else if (opponent == "earth") {
-                opponentScore += 1;
-            } 
-        }
-        else if (you == "earth") {
-            if (opponent == "metal") {
-                opponentScore += 1;
-            }
-            else if (opponent == "Water") {
-                yourScore += 1;
-            }
-            else if (opponent == "wood") {
-                opponentScore += 1;
-            }
-            else if (opponent == "fire") {
-                yourScore += 1;
-            } 
+            }                
+         } 
 
-    document.getElementById("your-score").innerText = yourScore;
-    document.getElementById("opponent-score").innerText = opponentScore;
+        if (you == "Wood") {
+            if (opponent == "metal") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "water") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "fire") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "earth") {
+                yourScore += 1;
+            }               
+        }
+        
+        if (you == "Water") {
+            if (opponent == "metal") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "Wood") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "fire") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "earth") {
+                opponentScore += 1;
+            }                
+        } 
+
+        if (you == "fire") {
+            if (opponent == "metal") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "Water") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "wood") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "earth") {
+                opponentScore += 1;
+            }
+        }
+                
+        if (you == "earth") {
+            if (opponent == "metal") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "Water") {
+                yourScore += 1;
+            }
+            else 
+            if (opponent == "wood") {
+                opponentScore += 1;
+            }
+            else 
+            if (opponent == "fire") {
+                yourScore += 1;
+            }              
+        }
+
     }
-}
 
+   
+
+
+document.getElementById("your-score").innerText = yourScore;
+document.getElementById("opponent-score").innerText = opponentScore;
+ 
+
+}
